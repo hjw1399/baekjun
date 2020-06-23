12945fda54a62b45254654ef9486e9d223adc114
jwlaptop <hjw1399@gmail.com>
2020-06-23 16:21:02 +0900

    2108 bug fixed : count logic was improved.

--- 2108_statistics/2108_statistics.cpp
+++ 2108_statistics/2108_statistics.cpp
@@ -43,32 +43,31 @@ int mode_index(int *data)
 	unsigned int remember_max_count = 1; 
 	unsigned int mode_index = 0; 
 	unsigned int count = 1;
-	std::vector<unsigned int> same_frequency_indices;
 	bool isFound2st = false;
-	for (size_t i = 0; i < length; i++)
+	for (size_t i = 0; i < length - 1; i++) // We must make it one less than the length
 	{
 		if (data[i] == data[i + 1])
 		{
 			count++;
-			if (!isFound2st)
-			{
-				remember_max_count = count; // At first, remember_max_count has to go up together.
-			}
 		}
 		else
 		{
-			if (count > remember_max_count)
-			{
-				mode_index = i;
-				remember_max_count = count;
-			}
-			else if (count == remember_max_count && !isFound2st)
-			{ 
-				mode_index = i + 1;
-				isFound2st = true;
-			}
 			count = 1;
 		}
+
+		if (count > remember_max_count)
+		{
+			mode_index = i;
+			remember_max_count = count;
+			isFound2st = false;
+		}
+		else if (count == remember_max_count && !isFound2st)
+		{ 
+			mode_index = i + 1;
+			isFound2st = true;
+		}
+		
+		
 	}
 
 	return mode_index;
@@ -81,7 +80,9 @@ int Ragne(int *data)
 	#elif linux
 		unsigned int length = malloc_usable_size(data) / sizeof(int);
 	#endif
+
 	return data[length - 1] - data[0];
+	
 }
 
 int main()
@@ -106,4 +107,11 @@ int main()
 	
 
 	delete[] data;
+
+	return 0;
 }
+
+//if (!isFound2st)
+//{
+//	remember_max_count = count; // At first, remember_max_count has to go up together.
+//}
