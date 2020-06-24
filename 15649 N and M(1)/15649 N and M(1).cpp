bb64ba6d2044f5e19ae9953f1da6236303e9787a
J.W.HAN <hjw1399@gmail.com>
2020-06-25 02:07:53 +0900

    permitation, practice back tracking

---
+++ 15649 N and M(1)/15649 N and M(1).cpp
@@ -0,0 +1,30 @@
+﻿// 15649 N and M(1).cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+	if we visit one, and we will never visit.
+	It's the backtracking method.
+*/
+
+vector<int> Permutation;
+
+void loadPermutation(const int& M, const int& N)
+{
+	/*if (Permutation.size() == N)
+	{
+
+	}*/
+}
+
+int main()
+{
+	int M, N;
+	cin >> M >> N;
+
+}
